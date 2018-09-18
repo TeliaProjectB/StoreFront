7921e9f295c34ea75a63c0161df499c8a0871403
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-09-18 15:01:42 +0200

    Implemented the new page structure for api page

---
+++ api/content.php
@@ -0,0 +1,56 @@
+ 
+    	<!--Real content start-->
+		<div id="contentContainer">
+			<div id="sideBar">
+				<div id="sideBarRealContent">
+					<div id="apiIcon" class="panel3d panel">
+						<h1>Apple API</h1>
+					</div>
+					<aside id="miniInfoBuyPanel" class="panel3d panel flexColumn">
+
+						<div><h4>Price:</h4> 5000kr</div>
+						
+						<div><h4>Category:</h4> Apple stuff</div>
+						<div><h4>Last updated:</h4> 2018-02-00</div>
+						<div id="thumbsUpDownContainer">
+							<div id="thumbsUp" class="thumbs"></div>
+							<div id="thumbsDown" class="thumbs"></div>
+						</div>
+						<button id="purchaseButton">Add to cart</button>
+					</aside>
+				</div>
+				
+			</div>
+			<div id="CenterContent">
+				<article id="info1" class="panel3d panel">
+					<h2>Information</h2>
+					<p>Telia Sverige AB (tidigare Telia Sonera Sverige AB, Telia AB och dessförinnan Televerket), är Sveriges största teleoperatör, både sett till omsättningen och antal kunder. <br><br>Företaget säljer anslutningar inom fast telefoni, datakommunikation, Internet, digital-tv, Ip-telefoni och mobiltelefoni till privatpersoner, företag och organisationer. Telia Sverige ingår i koncernen Telia Company. <br><br>Telia Company använder varumärket Telia i sin marknadsföring till slutkunder på den svenska marknaden. Andra varumärken inkluderar Halebop och Skanova (som sedan 2007 också är ett företag inom samma koncern, med tjänster riktade till andra teleoperatörer).</p>
+				</article>
+				<div id="panelsContainer" class="panel3d panel flexColumn">
+					<div id="rowButtonContainer">
+						<button class="rowButton" id="infoButton">Info</button>
+						<button class="rowButton"  id="sandboxButton">Sandbox</button>
+						<button class="rowButton"  id="commentsButton">Comment</button>
+						<button class="rowButton"  id="recommendedButton">Recommended</button>
+					</div>
+					<div id="infoPanel">
+						<p>Name: Apple API<br>
+						Example request urls:<br>
+						Request apple: apple.com/request/?type=apple&number=5
+						<br>
+						Returns an Apple JSON.
+						</p>
+					</div>
+					<div id="sandboxPanel">
+						<div id="sandboxArea"></div>
+					</div>
+					<div id="commentPanel">
+						<div id="commentArea"></div>
+					</div>
+					<div id="recommendPanel">
+						<p>Recommended panel</p>
+					</div>
+				</div>
+			</div>
+		</div>
+    	<!--Real content end-->
---
+++ api/customCss.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" type="text/css" href="css/commentPanelStyle.css">
+<link rel="stylesheet" type="text/css" href="css/sandbox.css">
+<link rel="stylesheet" type="text/css" href="css/thumbsStyle.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
