cce94e33801e3a95924b0a4a361cd02154a92414
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-09-18 13:43:34 +0200

    Add files via upload

---
+++ scripts/loginSidebar.js
@@ -0,0 +1,8 @@
+function open_sidebar() {
+    document.getElementById("mySidenav").style.width = "180px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+function close_sidebar() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
