6fbdfe930f40902f1f3b7d283df240e68c050424
dara1401 <dara1401@student.miun.se>
2018-09-19 14:02:49 +0200

    Add files via upload
    login window

--- pageStructure/scripts/loginSlidingWindow.js
+++ pageStructure/scripts/loginSlidingWindow.js
@@ -12,3 +12,17 @@ function openLoginWindow(){
     $("#firstStep").css({"display": "none"});
     $("#loginWindow").css({"display": "inline"});
 }
+
+function checkUser(){
+    var username = $('#username').val();
+    var password = $('#password').val();
+
+    if(username == "hi" && password == "123"){
+        $("#loginWindow").css({"display": "none"});
+        $("#signedIn").css({"display": "inline"});
+        
+    }
+    else{
+        console.log("Username: ", username)
+    }
+}
