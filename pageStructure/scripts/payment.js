d405a426e722db4f526a27778952b69562459201
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-09-21 11:35:15 +0200

    old

--- pageStructure/scripts/payment.js
+++
@@ -1,8 +0,0 @@
-function startFunction() {
-    var v = document.getElementById("myDIV10");
-    if (v.style.display === "none") {
-        v.style.display = "block";
-    } else {
-        v.style.display = "none";
-    }
-}
