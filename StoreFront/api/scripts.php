ed928f67f50ec058e20b6674aac47a76cef0cbe8
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-09-21 10:34:20 +0200

    Delete scripts.php

--- StoreFront/api/scripts.php
+++
@@ -1,9 +0,0 @@
-<script src="scripts/scrollChecker.js"></script>
-    <script src="scripts/fancyClicking.js"></script>
-    <script src="scripts/panelSwitcher.js"></script>
-	<script src="scripts/commentSystem.js"></script>
-	<script src="scripts/sandboxCreator.js"></script>
-	<script>
-		createMessageBox("commentArea");
-		createSandbox("sandboxArea");
-	</script>
