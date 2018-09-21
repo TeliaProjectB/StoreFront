dbf27f77c41646d77a44f1f5dbe008a7a8a46d51
dara1401 <dara1401@student.miun.se>
2018-09-21 14:06:14 +0200

    Add files via upload
    category function

--- home/content.php
+++ home/content.php
@@ -8,7 +8,7 @@ include $_SERVER["DOCUMENT_ROOT"]."/StoreFront/homeSplashPage/content.php";
 <div class="contentRow" name="Top list"></div>
 
 
-<div class="contentRow" name="Free"></div>
+<div id="freeCategory" class="contentRow" name="Free"></div>
 
 
-<div class="contentRow" name="Paid"></div>
+<div id="paidCategory" class="contentRow" name="Paid"></div>
