0a471d0658f7faef0959f0987422a3d319a24497
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-09-21 10:40:31 +0200

    Delete main.js

--- StoreFront/home/scripts/main.js
+++
@@ -1,8 +0,0 @@
-requirejs(["rowCreator"], function(rowCreator) {
-	var createRows = new rowCreator.init();
-	var allContentRows = document.getElementsByClassName("contentRow");
-
-	for(var i=0; i<allContentRows.length; i++){
-		createRows.appendRow(allContentRows[i]);
-	}
-});
