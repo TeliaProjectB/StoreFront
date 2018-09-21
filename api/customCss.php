67716f465b1712e67f8b5eefe9d078d64190d084
dara1401 <dara1401@student.miun.se>
2018-09-21 10:21:44 +0200

    Add files via upload

--- api/customCss.php
+++ api/customCss.php
@@ -2,3 +2,4 @@
 <link rel="stylesheet" type="text/css" href="css/sandbox.css">
 <link rel="stylesheet" type="text/css" href="css/thumbsStyle.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/infobox.css">
