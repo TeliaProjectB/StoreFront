9c11299f7e873f1ee106c1493c0f4b8b41f80ab7
dara1401 <dara1401@student.miun.se>
2018-09-24 10:08:11 +0200

    Add files via upload

---
+++ myAccount/scripts/Account.js
@@ -0,0 +1,41 @@
+			function showHideDiv1(ele) {
+				var srcElement = document.getElementById(ele);
+				if (srcElement != null) {
+					if (srcElement.style.display == "block") {
+						srcElement.style.display = 'none';
+					}
+					else {
+						srcElement.style.display = 'block';
+					}
+					return false;
+				}
+			}
+			
+					function howHideDiv2(ele) {
+				var srcElement = document.getElementById(ele);
+				if (srcElement != null) {
+					if (srcElement.style.display == "block") {
+						srcElement.style.display = 'none';
+					}
+					else {
+						srcElement.style.display = 'block';
+					}
+					return false;
+				}
+			}
+			
+			
+					function owHideDiv3(ele) {
+				var srcElement = document.getElementById(ele);
+				if (srcElement != null) {
+					if (srcElement.style.display == "block") {
+						srcElement.style.display = 'none';
+					}
+					else {
+						srcElement.style.display = 'block';
+					}
+					return false;
+				}
+			}
+			
+			
