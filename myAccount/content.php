9e3f393d920a3a93726445b72a40d975ffdf4c03
Skylightreza <43273686+Skylightreza@users.noreply.github.com>
2018-09-26 15:03:33 +0200

    Add files via upload

--- myAccount/content.php
+++ myAccount/content.php
@@ -1,104 +1,53 @@
-<div id="ivMsg2"style="display:none" class="Table" >
-
-<p class="three1">
-
-    <div class="Title">
-	
-        
-    </div>
-    <div class="Heading">
-        <div class="Cell">
-            <p>API</p>
-        </div>
-		<div class="Cell">
-            <p>Quantity</p>
-        </div>
-        <div class="Cell">
-        <p>Descraption</p>
-        </div>
-        <div class="Cell">
-            <p>Price</p>
-        </div>
-		 <div class="Cell">
-            <p>Date</p>
-        </div>
-    </div>
-    <div class="Row">
-        <div class="Cell">
-            <p>Row 1 Column 1</p>
-        </div>
-        <div class="Cell">
-            <p>Row 1 Column 2</p>
-        </div>
-        <div class="Cell">
-            <p>Voice-Message</p>
-        </div>
-		<div class="Cell">
-            <p>Row 1 Column 4</p>
-        </div>
-		<div class="Cell">
-            <p>Row 1 Column 5</p>
-        </div>
-		
-    </div>
-    <div class="Row">
-        <div class="Cell">
-            <p>Row 2 Column 1</p>
-        </div>
-        <div class="Cell">
-            <p>Row 2 Column 2</p>
-        </div>
-        <div class="Cell">
-            <p>SMS-message</p>
-        </div>
-		<div class="Cell">
-            <p>Row 2 Column 4</p>
-        </div>
-		<div class="Cell">
-            <p>Row 2 Column 5</p>
-        </div>
-    </div>
-</div>
-</div>
-
-
-
-<div id="divMsg1"style="display:none">
-
-<p class="three">
-<div id="myDIV">
-<body>
-
-
-
-   <p>Name:</p>
-   <p>Lastname:</p>
-   <p>E-mail:</p>
-   <p>Phone:</p>
-   <p>Country:</p>
-   <p>Gender:</p>
-   </body>
-        </div>
-    </div>
-</div>
-</div>
-
-
-
-
- 
-<div id="vMsg3"style="display:none">
-
-<p class="three2">
-
-
-<a href="#" class="button111">Credit card</a>
-<a href="#" class="button111">Paypal</a>
-      </div>
-    </div>
-
+		<link rel="stylesheet" type="text/css"  href="/StoreFront/myAccount/css/myAccount.css">
+		<div class="w.s.t">
+		  <h1>Hi user</h1>
+		   </div>
+			<div class="account">
+			  <input id="account-one" type="checkbox" name="tabs">
+			  <label for="account-one">Information</label>
+			  <div class="account-content">
+				<p>Lastname</p>
+				<p>Username</p>
+				<p>Gender</p>
+				<p>E-mail</p>
+				<p>Company ID</p>
+				
 		</div>
-				</div>
-			</div>
-		
+		</div>
+		<div class="account">
+		<input id="account-two" type="checkbox" name="tabs">
+		<label for="account-two">Bought items</label>
+		<div class="account-content">
+		 <table class="tableInfoBox">
+		<tr>
+		<th>API</th>
+		<th>Price</th>
+		<th>Bought</th>
+		<th>Time</th>
+		</tr>
+		 <tr>
+		<td>Test API</td>
+		<td>200kr</td>
+		<td>2018-09-11</td>
+		<td>19:30</td>
+		</tr>
+		</table>
+		</div>
+		</div>
+				<div class="account">
+			  <input id="account-tree" type="checkbox" name="tabs">
+			  <label for="account-tree">Payment</label>
+			  <div class="account-content">
+			  <a href="#" class="button">Credit card</a>
+              <a href="#" class="button">paypal</a>
+			
+		 </div>
+		</div>
+				<div class="account">
+			  <input id="account-four" type="checkbox" name="tabs">
+			  <label for="account-four">Log out</label>
+		</div>
+		</div>
+				
+
 
