b486f5e35e4c20fa6594c4eef7a0a30c058b7d35
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-09-26 11:56:23 +0200

    Delete customCss.php

--- StoreFront/api/customCss.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" type="text/css" href="css/commentPanelStyle.css">
-<link rel="stylesheet" type="text/css" href="css/sandbox.css">
-<link rel="stylesheet" type="text/css" href="css/thumbsStyle.css">
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<link rel="stylesheet" type="text/css" href="css/infobox.css">
