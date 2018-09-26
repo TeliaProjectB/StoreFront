6d051338658af3e891cefe310ebb74ceac734196
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-09-26 14:37:33 +0200

    Small changes

--- homeSplashPage/content.php
+++ homeSplashPage/content.php
@@ -4,10 +4,12 @@
     <div class="splashMiddleContent">
         <div class="flyingEnvelope"></div>
         <div class="SplashTextInfoContainer">
-            <div class="splashTitle">Recommended for you</div>
-            <div class="splashSubTitle">Sms API</div>
-            <div class="splashDescription">
-                Being able send sms to alert your users can be a very powerfull tool. With this API it is noweasier than ever! 
+            <div class="splashinfoFlexCon">
+                <div class="splashTitle">Recommended for you</div>
+                <div class="splashSubTitle">Sms API</div>
+                <div class="splashDescription">
+                    Being able send sms to alert your users can be a very powerfull tool. With this API it is noweasier than ever! 
+                </div>
             </div>
         </div>
         
