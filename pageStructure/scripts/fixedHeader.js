c714b2fb618f1d308a72ba01d62e82c04765d8bf
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-10-01 13:35:51 +0200

    Add files via upload

--- pageStructure/scripts/fixedHeader.js
+++ pageStructure/scripts/fixedHeader.js
@@ -1,4 +1,4 @@
-/* function for header fixed */
+/* function for header fixed 
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("head");
@@ -6,5 +6,28 @@ var header = document.getElementById("head");
 function myFunction() {
     var top  = window.pageYOffset || document.documentElement.scrollTop;
     header.style.marginTop = top + "px";
+    if($('.loginWindow:visible').length != 0){
+        $(".loginWindow").css({display:"none"});
+    }
    
+}
+*/
+/*info telia dropdown */
+function openInfoTelia(){
+    $("#infoTelia").animate({height: 'toggle'});
+
+}
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("head");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
