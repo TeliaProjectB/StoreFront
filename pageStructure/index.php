7c15b1ba95220fd4bf062cb3c50fd244b7306e1b
dara1401 <dara1401@student.miun.se>
2018-10-01 11:08:35 +0200

    Add files via upload
    removed secound window

--- pageStructure/index.php
+++ pageStructure/index.php
@@ -71,28 +71,13 @@
 
 					<div id="signedIn">
 						<a href="#" onclick="window.open('/StoreFront/myAccount','_self')" class="sidenavText">Account</a>
-						<a href="#" onclick="openSecondWindow()" class="sidenavText">Bought items</a>
+						<a href="#" onclick="window.open('/StoreFront/myAccount','_self')" class="sidenavText">Bought items</a>
 						<a href="#" class="sidenavText">Payment</a>
 						<a href="#" onclick="goBackToStart()" class="sidenavText">Log out</a>	
 					</div>
 				</div>
 			</div>
 			
-			<div class="secondWindow">
-				<div>Bought Items</div>
-				<table class="tableInfoBox">
- 					<tr>
- 						<th>API</th>
-						<th>Price</th>
-						<th>Bought</th>
-					 </tr>
-					 <tr>
- 						<td>Test API</td>
-						<td>200kr</td>
-						<td>2018-09-11</td>
-					</tr>
-				</table>
-			</div>
 			
 			<div id="contentID">
 				<?php
