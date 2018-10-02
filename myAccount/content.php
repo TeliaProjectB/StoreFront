ce9fcb56eab260d7ad9eea097f780e8026633297
Skylightreza <43273686+Skylightreza@users.noreply.github.com>
2018-10-02 12:52:59 +0200

    Add files via upload

--- myAccount/content.php
+++ myAccount/content.php
@@ -1,24 +1,30 @@
 		<link rel="stylesheet" type="text/css"  href="/StoreFront/myAccount/css/myAccount.css">
 		<div class="w.s.t">
-		  <h1>Hi user</h1>
+		  <h1></h1>
 		   </div>
 			<div class="account">
 			  <input id="account-one" type="checkbox" name="tabs">
-			  <label for="account-one">Information</label>
-			  <div class="account-content">
-				<p>Lastname</p>
+			  <label for="account-one" class="headerButton1">Info</label>
+			  <div class="info">
+                <p>Lastname</p>
 				<p>Username</p>
 				<p>Gender</p>
 				<p>E-mail</p>
 				<p>Company ID</p>
+</div>
+				
+				
+		
+		
 				
 		</div>
 		</div>
 		<div class="account">
 		<input id="account-two" type="checkbox" name="tabs">
-		<label for="account-two">Bought items</label>
+		<label for="account-two" class="headerButton2">Bought Items</label>
 		<div class="account-content">
 		 <table class="tableInfoBox">
+		 <br>
 		<tr>
 		<th>API</th>
 		<th>Price</th>
@@ -34,20 +40,33 @@
 		</table>
 		</div>
 		</div>
+		
 				<div class="account">
 			  <input id="account-tree" type="checkbox" name="tabs">
-			  <label for="account-tree">Payment</label>
+			  <label for="account-tree"class="headerButton3">Peyment</label>
 			  <div class="account-content">
-			  <a href="#" class="button">Credit card</a>
-              <a href="#" class="button">paypal</a>
+			  <a href="#" class="button headerButton3" >Credit card</a>
+              <a href="#" class="button headerButton3">Paypal</a>
 			
 		 </div>
 		</div>
-				<div class="account">
+		
+		      <div class="account">
 			  <input id="account-four" type="checkbox" name="tabs">
-			  <label for="account-four">Log out</label>
+			  <label for="account-four" class="headerButton4">Delete Account</label>
 		</div>
 		</div>
+		
+				
+			 
+			
+			  
+			  
+			  
+		</div>
+		</div>
+		</br>
+		
 				
 
-
+				
