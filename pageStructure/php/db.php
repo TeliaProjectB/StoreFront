08f81932491bb7c366dbb0fcfee4b82193d5a6bc
dara1401 <dara1401@student.miun.se>
2018-10-02 11:31:55 +0200

    Add files via upload
    new version to able to connect to the server

--- pageStructure/php/db.php
+++ pageStructure/php/db.php
@@ -1,8 +1,20 @@
 <?php
     /* Database connection settings */
-    $host = '10.11.0.7'; // Ip-address 
-    $user = 'root';
-    $pass = 'password';
-    $db = 'accounts'; //Which database
-    //$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+    $servername = '10.11.3.71';
+    $username = 'testUser';
+    $password = 'RnuUXp3k0jDX0DR1';
+    $dbname = 'telia';
+
+    //8UF5J264pK19KxVK
+
+    
+    // Create connection
+    $mysql = mysql_connect($servername, $username, $password);
+    //$conn = new mysqli($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if (!$mysql) {
+        die("Connection failed: ");
+    } 
+
 ?>
