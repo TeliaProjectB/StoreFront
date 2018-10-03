30909b39457969a24cabc716ef0c86019cf01b95
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-10-03 10:31:17 +0200

    Add files via upload

--- pageStructure/scripts/scrollDownFunction.js
+++ pageStructure/scripts/scrollDownFunction.js
@@ -1,7 +1,16 @@
 function scrollDownTo(nameOfTag){
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#" + nameOfTag).offset().top
-    }, 1000);
+
+    var pathname = window.location.href;
+    if (pathname == "http://localhost/StoreFront/home/") { /* change with window href*/
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#" + nameOfTag).offset().top
+        }, 1000);
+    }
+    else {
+        window.open('/StoreFront/search','_self'); /*change depending on the category */
+    }
+    
+   
 }
 
 /* dropdown scroll */
