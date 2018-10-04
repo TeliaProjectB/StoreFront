c466d610255e90aaf19bc08234dd67824f20cd15
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-10-04 15:28:33 +0200

    Add files via upload

--- home/content.php
+++ home/content.php
@@ -13,13 +13,14 @@ include $_SERVER["DOCUMENT_ROOT"]."/StoreFront/homeSplashPage/content.php";
 <div id="freeCategory" class="contentRow" name="Free"></div>
 
 
-<div class="bigBoxesCategories" name="for company" id="Statistics API" maximum="3"></div>
+<div class="bigBoxesCategories" name="For Company" id="Statistics API" maximum="3"></div>
 
+<div id="callCategory" class="contentRow" name="Call"></div>
+<div id="freeCategory" class="contentRow" name="SMS"></div>
 
+<div class="bigBoxesCategories" name="For Company" id="Call" maximum="6"></div>
 
-<div id="freeCategory" class="contentRow" name="sms"></div>
+<div id="recommendedCategory" class="contentRow" name="Recommended"></div>
+<div id="funCategory" class="contentRow" name="Fun"></div>
 
-
-<div class="bigBoxesCategories" name="for company" id="Call" maximum="6"></div>
-
-
+<div class="bigBoxesCategories" name="Most Recent" id="Statistics API" maximum="3"></div>
