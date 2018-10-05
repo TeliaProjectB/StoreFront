8598cdaca18b2257ad2d97cd5d729cb3cd2e4697
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-10-05 11:32:23 +0200

    Add files via upload
    Search bar goes only with name of categories, make better with name single API

--- pageStructure/scripts/scrollDownFunction.js
+++ pageStructure/scripts/scrollDownFunction.js
@@ -1,3 +1,6 @@
+
+/* scroll if click on category */
+
 function scrollDownTo(nameOfTag){
 
     var pathname = window.location.href;
@@ -13,7 +16,30 @@ function scrollDownTo(nameOfTag){
    
 }
 
-/* dropdown scroll */
+/* scroll if search category on the search bar */
+
+function searchCategory(){
+    
+    var searchInput = $('#searchInput').val();
+    var searchCat = document.getElementsByName(searchInput);
+    var id = searchCat[0].getAttribute('id');
+    
+    var pathname = window.location.href;
+    if (pathname == "http://localhost/StoreFront/home/") { /* change with window href*/
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#" + id).offset().top
+        }, 1000);
+    }
+    else {
+        window.open('/StoreFront/search','_self'); /*change depending on the category */
+    }
+    
+   
+}
+
+
+/* dropdown button other scroll*/
+
 function openCloseDropdown(){
   $(".dropdown-content").animate({height: 'toggle'});  
 }
