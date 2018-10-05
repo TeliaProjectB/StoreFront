5b10950877317cfc100b8e818d59a596c5457efa
dara1401 <dara1401@student.miun.se>
2018-10-05 10:26:26 +0200

    Add files via upload
    small changes in the register and sign Function

--- pageStructure/php/signinUserClass.php
+++ pageStructure/php/signinUserClass.php
@@ -1,18 +1,39 @@
 <?php
 
 class signinUser{
-    function checkUser($name, $password){
+    function checkUserFromSQL($name, $password){
+		require $_SERVER["DOCUMENT_ROOT"].'/StoreFront/pageStructure/php/db.php';
+
+		// AND User_pass = '$password'
+
+		$sql = "SELECT * FROM `user` WHERE User_name = '$name'";
+
+		try{
+			$result = $conn->query($sql);
+			$row = $result->fetch_assoc();
+
+			return password_verify($password , $row["User_pass"]); 
+		}
+		catch(Exception $e){
+			return false;
+		}
+
+		
+	}
+
+
+	function getInfoFromName($userName){
 		require $_SERVER["DOCUMENT_ROOT"].'/StoreFront/pageStructure/php/db.php';
 
 		$userPasswordHash = password_hash($password, PASSWORD_DEFAULT);
-		$sql = "SELECT * FROM `user` WHERE User_name = '$name' AND User_pass = '$password'";
 
-		try{
-			$conn->query($sql);
-            return $sql;
-            
-		}catch(Exception $e){
-			return false;
-		}
+		$sql = "SELECT * from `user` where `User_name`='$userName'";
+
+		$result = $conn->query($sql);
+
+		$row = $result->fetch_assoc();
+
+		return $row;
 	}
+}
 ?>
