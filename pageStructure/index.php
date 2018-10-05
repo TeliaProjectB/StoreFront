0bb02eb4130498f26778de729d7c2b0f3d31b423
Skylightreza <43273686+Skylightreza@users.noreply.github.com>
2018-10-05 10:38:15 +0200

    Add files via upload

--- pageStructure/index.php
+++ pageStructure/index.php
@@ -128,6 +128,7 @@
 							<a href="#" onclick="window.open('/StoreFront/myAccount?index=info','_self')" class="sidenavText">Account</a>
 							<a href="#" onclick="window.open('/StoreFront/myAccount?index=boughtItems','_self')" class="sidenavText">Bought items</a>
 							<a href="#" onclick="window.open('/StoreFront/myAccount?index=payment','_self')" class="sidenavText">Payment</a>
+							<a href="#" onclick="goBackToStart()" class="sidenavText">Delete Account</a>	
 							<a href="#" onclick="goBackToStart()" class="sidenavText">Log out</a>	
 						</div>
 						
