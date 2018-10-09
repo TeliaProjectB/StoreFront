aa8e296213114cfa5323c485ddb0808827f33cb7
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-10-09 15:35:10 +0200

    Delete customCss.php

--- search/customCss.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" type="text/css" href="css/rowStyle.css">
-<link rel="stylesheet" type="text/css" href="css/simpleBoxContainer.css">
-<link rel="stylesheet" type="text/css" href="css/loaderStyle.css">
