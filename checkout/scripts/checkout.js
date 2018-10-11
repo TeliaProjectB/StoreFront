8a535b83340def36eaa8fc6088b89b31b0e55d40
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-10-11 13:02:14 +0200

    Update checkout.js

--- checkout/scripts/checkout.js
+++ checkout/scripts/checkout.js
@@ -10,6 +10,11 @@ function removeApiFromCart(apiId, element){
 			element.style.opacity = "0";
 			setTimeout(function(){
 				element.parentElement.removeChild(element);
+
+				var trolleyCounter = document.getElementById("itemCount");
+				console.log(trolleyCounter);
+				trolleyCounter.innerHTML = parseInt(trolleyCounter.innerHTML, 10)-1;
+
 			}, 700);
 			
 		}
@@ -27,6 +32,7 @@ function purchase(){
 		if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
 			console.log(this.responseText);
 			alert("Success! Youare now the owner of these API.");
+			window.open("/StoreFront/myAccount/items/?index=boughtItems", "_self");
 		}
 	}
 	xhr.send("");
