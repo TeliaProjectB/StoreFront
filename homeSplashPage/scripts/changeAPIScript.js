33c9a932e31aa27128152caac31c5d5650273ad1
dara1401 <dara1401@student.miun.se>
2018-10-15 15:14:11 +0200

    Add files via upload
    navigation button in homesplashwindow

--- homeSplashPage/scripts/changeAPIScript.js
+++ homeSplashPage/scripts/changeAPIScript.js
@@ -1,6 +1,18 @@
 var myIndex = 0;
 carousel();
 
+function changeCurrentButton(myIndex){
+    console.log("CurrentIndex: ", myIndex)
+    var x = document.getElementsByClassName("splashNavButton");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.backgroundColor = "gray"; 
+    }
+
+    var button = document.getElementById("button" + myIndex );
+    button.style.border = "solid 1px gray";
+    button.style.backgroundColor = "white";
+}
+
 function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
@@ -11,6 +23,25 @@ function carousel() {
     if (myIndex > x.length) {
         myIndex = 1
     }    
-    x[myIndex-1].style.display = "block";  
+    x[myIndex-1].style.display = "block";
+    
+    changeCurrentButton(myIndex);
     setTimeout(carousel, 6000);    
+}
+
+
+
+function goToAPI(goToThisAPI){
+    console.log("API nr: ", goToThisAPI);
+    myIndex = goToThisAPI;
+    
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+
+        
+    x[myIndex-1].style.display = "block";
+    changeCurrentButton(myIndex);
 }
