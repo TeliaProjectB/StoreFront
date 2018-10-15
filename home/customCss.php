52fa6e675a2544a58d8d7fa2176d2cd4c8cf67df
dara1401 <dara1401@student.miun.se>
2018-10-15 14:58:16 +0200

    Update customCss.php

--- home/customCss.php
+++ home/customCss.php
@@ -1,4 +1,3 @@
 <link rel="stylesheet" type="text/css" href="css/rowStyle.css">
 <link rel="stylesheet" type="text/css" href="css/simpleBoxContainer.css">
 <link rel="stylesheet" type="text/css" href="css/loaderStyle.css">
-<link rel="stylesheet" type="text/css" href="/StoreFront/homeSplashPage/splashStyle.css">
