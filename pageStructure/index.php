2b6d189c61b0cfd72970726a8b8aabca2567cd28
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-10-16 13:16:36 +0200

    Add files via upload

--- pageStructure/index.php
+++ pageStructure/index.php
@@ -96,33 +96,9 @@
 				<!-- HELP WINDOW -->
 				<div class="helpWindow">
 					<div class="posisionOfTheHelpWindow">
-								
-						<div id="helpWindow">
-							<div id="infoTeliaUp">
-								<p>© Telia Sverige AB 556430-0142<br>
-								<p>Box 50077, 973 22 Luleå<br>
-								<p>Säte: Stockholm
-							</div>
-
-							<div id="mainQuestions">
-								<a href="#" onclick="openAnswer(this.id)" class="questionText" id="question1"></a>
-								<div class="answerText" id="answer1">
-								</div>
-								<a href="#" onclick="openAnswer(this.id)" class="questionText" id="question2"></a>
-								<div class="answerText" id="answer2">
-								</div>
-								<a href="#" onclick="openAnswer(this.id)" class="questionText" id="question3"></a>
-								<div class="answerText" id="answer3">
-								</div>
-								<a href="#" onclick="openAnswer(this.id)" class="questionText" id="question4"></a>
-								<div class="answerText" id="answer4">
-								</div>
-								<a href="#" onclick="openAnswer(this.id)" class="questionText" id="question5"></a>
-								<div class="answerText" id="answer5">
-								</div>
-							</div>
-							<button class="closeWindowButton" onclick="openHelpWindow()"></button>
-						</div>
+						<?php
+							require $_SERVER["DOCUMENT_ROOT"].'/StoreFront/pageStructure/panelParts/helpWindow.php';
+						?>	
 					</div>
 				</div>
 
---
+++ pageStructure/panelParts/helpWindow.php
@@ -0,0 +1,22 @@
+<div id="helpWindow">
+	<div id="infoTeliaUp">
+		<p>© Telia Sverige AB 556430-0142<br>
+		<p>Box 50077, 973 22 Luleå<br>
+		<p>Säte: Stockholm
+    </div>
+
+	<div id="mainQuestions">
+		<a href="#" onclick="openAnswer(this.id)" class="questionText" id="question1"></a>
+		<div class="answerText" id="answer1"></div>
+		<a href="#" onclick="openAnswer(this.id)" class="questionText" id="question2"></a>
+		<div class="answerText" id="answer2"></div>
+		<a href="#" onclick="openAnswer(this.id)" class="questionText" id="question3"></a>
+		<div class="answerText" id="answer3"></div>
+		<a href="#" onclick="openAnswer(this.id)" class="questionText" id="question4"></a>
+		<div class="answerText" id="answer4"></div>
+		<a href="#" onclick="openAnswer(this.id)" class="questionText" id="question5"></a>
+		<div class="answerText" id="answer5"></div>
+	</div>
+    
+    <button class="closeWindowButton" onclick="openHelpWindow()"></button>
+</div>
