486b6171d86dbd1dae2a1c828320c77a784c7376
Skylightreza <43273686+Skylightreza@users.noreply.github.com>
2018-10-18 19:24:38 +0200

    Delete signedIn.php

--- pageStructure/panelParts/signedIn.php
+++
@@ -1,7 +0,0 @@
-<div id="signedIn">
-	<a href="#" onclick="window.open('/StoreFront/myAccount?index=info','_self')" class="sidenavText">Account</a>
-	<a href="#" onclick="window.open('/StoreFront/myAccount/items/?index=boughtItems','_self')" class="sidenavText">Bought items</a>
-	<a href="#" onclick="window.open('/StoreFront/myAccount/pay/?index=payment','_self')" class="sidenavText">Payment</a>
-	<!--<a href="#" onclick="goBackToStart()" class="sidenavText">Delete Account</a>-->	
-	<a href="#" onclick="userSignOut()" class="sidenavText">Log out</a>	
-</div>
