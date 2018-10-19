08a5de251ff904825a411b4df3cb025a979c0abd
dara1401 <dara1401@student.miun.se>
2018-10-19 09:22:39 +0200

    Add files via upload

--- terms.php
+++ terms.php
@@ -1,7 +1,7 @@
 <?php
     echo "
         <div style='text-align: center;'>
-            <img src='/StoreFront/globalImages/devi.png'>
+            <img src='/StoreFront/globalImages/Devi.png'>
             <h1>You have now sold your soul to Telia</h1>
         </div>
         ";
