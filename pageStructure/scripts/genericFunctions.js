93c276cd4fa5a2e8707cf95fbe7de3cd15bedb1d
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-10-20 14:07:40 +0200

    Update genericFunctions.js

--- pageStructure/scripts/genericFunctions.js
+++ pageStructure/scripts/genericFunctions.js
@@ -1,11 +1,26 @@
 var documentCTRLKeyIsActivated = false;
 document.addEventListener("keydown", function(e){
-	if(e.keyCode == 17){
+	var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+	if(isSafari){
+		if(e.keyCode == 91){
+			documentCTRLKeyIsActivated = true;
+			break;
+		}
+	}
+	if(e.keyCode == 17){//ctrl
 		documentCTRLKeyIsActivated = true;
 	}
 });
 document.addEventListener("keyup", function(e){
-	if(e.keyCode == 17){
+	var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+	if(isSafari){
+		if(e.keyCode == 91){
+			documentCTRLKeyIsActivated = false;
+			break;
+		}
+	}
+
+	if(e.keyCode == 17){//ctrl
 		documentCTRLKeyIsActivated = false;
 	}
 });
