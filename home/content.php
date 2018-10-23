f1ecf28e8033b280f75eba4151d4336ca96dce89
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-10-23 16:04:43 +0200

    Add files via upload

--- home/content.php
+++ home/content.php
@@ -22,19 +22,33 @@ include $_SERVER["DOCUMENT_ROOT"]."/StoreFront/homeSplashPage/content.php";
 	</div>
 </div>
 
-<div class="bigBoxesCategories" name="Most liked" id="mostLikedCategories" maximum="3"></div>
+<!-- 6 categories in home, then 12 at the end with only the box containing the title of category -->
 
 <div id="callCategory" class="contentRow" name="Call"></div>
 <div id="smsCategory" class="contentRow" name="SMS"></div>
 
-<div class="bigBoxesCategories" name="For new started company" id="Call" maximum="6"></div>
+<div class="bigBoxesCategories" name="Most liked" id="mostLikedCategories" maximum="3"></div>
 
-<div id="recommendedCategory" class="contentRow" name="Recommended"></div>
 <div id="funCategory" class="contentRow" name="Fun"></div>
+<div id="mobileCategory" class="contentRow" name="Mobile"></div>
+
+<div class="bigBoxesCategories" name="For new started company" id="newCompanyCategories" maximum="6"></div>
+
+<div id="paymentCategory" class="contentRow" name="Payment"></div>
+<div id="StatisticsAPICategory" class="contentRow" name="Statistics API"></div>
 
 <div class="bigBoxesCategories" name="Most bought" id="mostBoughtCategory" maximum="3"></div>
 
-<div id="mobileCategory" class="contentRow" name="Mobile"></div>
-<div id="StatisticsAPICategory" class="contentRow" name="Statistics API"></div>
+<div id="employerCategory" class="contentRow" name="Employer"></div>
+<div id="managementCategory" class="contentRow" name="Management"></div>
+
+<div class="bigBoxesCategories" name="For company" id="companyCategories" maximum="6"></div>
+
+<div id="cloudCategory" class="contentRow" name="Cloud"></div>
+
+<div class="bigBoxesCategories" name="For industry" id="industryCategory" maximum="3"></div>
 
 <div id="allCategory" class="contentRow" name="All"></div>
+
+
+<!-- COMPLETE WITH SHOWING ALL THE SINGLE CATEGORIES IN BOXES -->
