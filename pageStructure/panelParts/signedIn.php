6530d10ecf18730e467d3f6a70250aafcc5e5936
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-10-23 11:57:22 +0200

    changed js links to href

--- pageStructure/panelParts/signedIn.php
+++ pageStructure/panelParts/signedIn.php
@@ -1,7 +1,7 @@
 <div id="signedIn">
-	<a href="#" onclick="smartJsLink('/StoreFront/profile?index=info')" class="sidenavText">Account</a>
-	<a href="#" onclick="smartJsLink('/StoreFront/profile/?index=boughtItems')" class="sidenavText">Bought items</a>
-	<a href="#" onclick="smartJsLink('/StoreFront/profile/?index=payment')" class="sidenavText">Payment</a>
+	<a href="/StoreFront/profile?index=info" class="sidenavText">Account</a>
+	<a href="/StoreFront/profile/?index=boughtItems" class="sidenavText">Bought items</a>
+	<a href="/StoreFront/profile/?index=payment" class="sidenavText">Payment</a>
 	<!--<a href="#" onclick="goBackToStart()" class="sidenavText">Delete Account</a>-->	
 	<a href="#" onclick="userSignOut()" class="sidenavText">Log out</a>	
 </div>
