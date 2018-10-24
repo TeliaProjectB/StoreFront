b86aca51e29318cafc4aa01117bebc2b998fe9a8
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-10-24 08:55:09 +0200

    Fied homepage categories and row css

--- home/content.php
+++ home/content.php
@@ -9,8 +9,17 @@ include $_SERVER["DOCUMENT_ROOT"]."/StoreFront/homeSplashPage/content.php";
 	<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
 </div>
 
+
 <div id="topListCategory" class="contentRow" name="Top List"></div>
 <div id="freeCategory" class="contentRow" name="Free"></div>
+
+<div class="bigBoxesCategories" name="Most liked" id="mostLikedCategories" maximum="3"></div>
+
+<!-- 6 categories in home, then 12 at the end with only the box containing the title of category -->
+
+<div id="managementCategory" class="contentRow" name="Management"></div>
+<div id="smsCategory" class="contentRow" name="SMS"></div>
+
 
 <div class="rowContainer">
 	<a href="/StoreFront/category?cat=Combined%API's" class="rowTitle">Combined API's</a>
@@ -22,31 +31,18 @@ include $_SERVER["DOCUMENT_ROOT"]."/StoreFront/homeSplashPage/content.php";
 	</div>
 </div>
 
-<!-- 6 categories in home, then 12 at the end with only the box containing the title of category -->
-
-<div id="callCategory" class="contentRow" name="Call"></div>
-<div id="smsCategory" class="contentRow" name="SMS"></div>
-
-<div class="bigBoxesCategories" name="Most liked" id="mostLikedCategories" maximum="3"></div>
-
 <div id="funCategory" class="contentRow" name="Fun"></div>
-<div id="mobileCategory" class="contentRow" name="Mobile"></div>
 
-<div class="bigBoxesCategories" name="For new started company" id="newCompanyCategories" maximum="6"></div>
+<!--<div class="bigBoxesCategories" name="For new started company" id="newCompanyCategories" maximum="6"></div>-->
 
-<div id="paymentCategory" class="contentRow" name="Payment"></div>
-<div id="StatisticsAPICategory" class="contentRow" name="Statistics API"></div>
+<!--<div id="StatisticsAPICategory" class="contentRow" name="Statistics API"></div>-->
 
 <div class="bigBoxesCategories" name="Most bought" id="mostBoughtCategory" maximum="3"></div>
 
-<div id="employerCategory" class="contentRow" name="Employer"></div>
-<div id="managementCategory" class="contentRow" name="Management"></div>
 
-<div class="bigBoxesCategories" name="For company" id="companyCategories" maximum="6"></div>
 
-<div id="cloudCategory" class="contentRow" name="Cloud"></div>
 
-<div class="bigBoxesCategories" name="For industry" id="industryCategory" maximum="3"></div>
+
 
 <div id="allCategory" class="contentRow" name="All"></div>
 
