c324fd4f432831f477b2f79e02c71e44fb7c0457
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-10-24 11:23:33 +0200

    Delete customCss.php

--- apiPackage/customCss.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" type="text/css" href="css/commentPanelStyle.css">
-<link rel="stylesheet" type="text/css" href="css/sandbox.css">
-<link rel="stylesheet" type="text/css" href="css/thumbsStyle.css">
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<link rel="stylesheet" type="text/css" href="css/infobox.css">
-<link rel="stylesheet" type="text/css" href="css/purchaseAnimation.css">
-<link rel="stylesheet" type="text/css" href="css/recApi.css">
