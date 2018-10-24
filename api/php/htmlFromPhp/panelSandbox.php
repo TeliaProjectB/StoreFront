a0e906f3e9e894b9b490fad99dbd11b27a8fa2e2
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-10-24 15:03:38 +0200

    Fixed styles

--- api/php/htmlFromPhp/panelSandbox.php
+++ api/php/htmlFromPhp/panelSandbox.php
@@ -1,7 +1,7 @@
 <div id="sandboxPanel">
 	<!--<div id="sandboxArea"></div>-->
 				
-	<div style="display:flex; flex-direction: row;">
+	<div style="display:flex; flex-direction: row; word-break: break-word;">
 
 		<div>
 			<div id="swaggerMinimize"></div>
@@ -13,7 +13,7 @@
 						
 		<div id="swaggerPatchInfo">
 			<p style="display:block;" id="apiSandboxInfo">
-				API(Application Programming Interface) gives you a way tocommunicate another program, service or database. They can have one or multiple functions that take in multipleparameters. Here you can try out different functions of this API. open a folder in the functions explorer and select a function.							
+				API(Application Programming Interface) gives you a way to communicate with another program, service or database. They can have one or multiple functions that take in multiple parameters. Here you can try out different functions of this API. Open a folder in the functions explorer and select a function.							
 			</p>
 			<div id="sandboxArrowLeft"></div>
 		</div>
