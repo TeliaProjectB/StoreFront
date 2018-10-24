34ce75c0a72b6955f68242b2211699fbb4497c63
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-10-24 11:29:26 +0200

    Delete fixHeader.js

--- myAccount_v2/scripts/fixHeader.js
+++
@@ -1,4 +0,0 @@
-(function(){
-	document.getElementById("secondHead").style.marginBottom = "0px";
-	document.getElementById("thirdHead").parentElement.removeChild(document.getElementById("thirdHead"));
-})();
