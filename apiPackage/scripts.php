bb152c24a57f407665376ac084f66e15247803c8
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-10-24 11:23:13 +0200

    Delete scripts.php

--- apiPackage/scripts.php
+++
@@ -1,6 +0,0 @@
-<script src="scripts/scrollChecker.js"></script>
-<script src="scripts/fancyClicking.js"></script>
-<script src="scripts/panelSwitcher.js"></script>
-<script src="scripts/addToCart.js"></script>
-<script src="scripts/handleLikeDislike.js"></script>
-<script data-main="scripts/commentSystem/main" src="scripts/commentSystem/require.min.js"></script>
