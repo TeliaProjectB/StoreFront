1cac2cdf4c224de920d55950bf98c37f25b40ef1
Bravos123 <40920234+Bravos123@users.noreply.github.com>
2018-10-25 09:15:42 +0200

    Add files via upload

--- pageStructure/index.php
+++ pageStructure/index.php
@@ -17,7 +17,7 @@
 <html>
 	<head>
 		<link rel="icon" href="/StoreFront/pageStructure/img/websiteIcon.png">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=0.5">
 		<meta charset="utf-8">
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		
