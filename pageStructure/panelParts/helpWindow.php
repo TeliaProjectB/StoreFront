6b62f1f5c252313f419abe123234237efb222443
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-10-30 09:36:33 +0100

    Add files via upload

--- pageStructure/panelParts/helpWindow.php
+++ pageStructure/panelParts/helpWindow.php
@@ -1,14 +1,16 @@
 <div id="helpWindow">
 	
-        <div id="contact1" class="strongButtonPanel" onclick="openContact(this.id)">
+        <div id="contact1" class="contactTelia" onclick="openContact(this.id)">
 			<p>Phone</p>
 		</div>
         <div class="contactInfoTelia" id="contactInformation1">
             <p>Fridays 08:00 - 19:00</p>
             <p> Phone number: 90 200</p>
             <p>From abroad: +46 771 99 02 00</p>
-        </div>	
-		
+        </div>
+		<br>	
+		<div id="contact2" class="contactTelia" onclick="openContact(this.id)">
+			<p>Auto. Support</p>
 		</div>
         <div class="contactInfoTelia" id="contactInformation2">
 			<p>Open Twentyfour Seven</p>
@@ -17,7 +19,7 @@
 			<p>From abroad: +46 771 41 10 00</p>
 		</div>
 		<br>
-		<div id="contact3" class="strongButtonPanel" onclick="openContact(this.id)">
+		<div id="contact3" class="contactTelia" onclick="openContact(this.id)">
 			<p>Chat</p>
 		</div>
         <div class="contactInfoTelia" id="contactInformation3">
@@ -41,8 +43,12 @@
 	</div>
 	<a href="mailto:grwe1400@student.miun.se?Subject=Hello%20again" target="_top" id="sendEmail">Send Mail</a>
 
-
+    <div id="infoTeliaUp">
+		<p>© Telia Sverige AB</p>
+		<p>Box 50077, 973 22 Luleå</p>
+		<p>Säte: Stockholm</p>
+    </div>
 	
     
-    <button class="closePanelButton" onclick="openHelpWindow()"></button>
+    <button class="closeWindowButton" onclick="openHelpWindow()"></button>
 </div>
