db927241ff913ea776be99325345237cacac2daa
Petta95 <43273276+Petta95@users.noreply.github.com>
2018-10-30 09:40:44 +0100

    Add files via upload

--- checkout/scripts/checkout.js
+++ checkout/scripts/checkout.js
@@ -32,16 +32,10 @@ function updatePrice(){
 
 	var newPrice = 0;
 	var prices = document.getElementsByClassName("apiRowPrice");
-	
 	for(var i=0; i<prices.length; i++){
-
-		prices[i].innerHTML = prices[i].innerHTML.replace(" ","");
-
 		newPrice += parseInt(prices[i].innerHTML, 10);
-		prices[i].innerHTML = prices[i].innerHTML.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " "); 
 	}
 
-	newPrice = newPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " "); 
 	priceContainer.innerHTML = "Total amount: "+newPrice+" kronor.";
 }
 
@@ -51,9 +45,9 @@ function purchase(){
 	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	xhr.onreadystatechange = function() {//Call a function when the state changes.
 		if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-			//console.log(this.responseText);
+			console.log(this.responseText);
 			alert("Success! You are now the owner of these API.");
-			window.open("/StoreFront/profile/?index=boughtItems", "_self");
+			window.open("/StoreFront/myAccount/?index=boughtItems", "_self");
 		}
 	}
 	xhr.send("");
